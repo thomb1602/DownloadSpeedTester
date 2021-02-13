c7e077c7a9cc2ddf3150be946025f30626c0705c
Tom Bates <thomas1602@hotmail.co.uk>
2021-02-13 22:13:20 +0000

    full output file path required, removed debug log

--- DownloadSpeedTester.ps1
+++ DownloadSpeedTester.ps1
@@ -2,19 +2,19 @@
 # Note that speedtest.exe must be at the same location as this file
 # Output.exe is assumed to already have header information, including date as first field
 
+# speedtest.exe is available here: https://www.speedtest.net/apps/cli
+
 Write-EventLog -LogName Application -Source "Speedtest" -EntryType Information -EventId 2 -Message "Speedtest starting"
 
-$resultsFile = "output.txt"
+# without the full file path the file will be saved in system32 if thi script is run by Task Scheduler
+$resultsFile = "C:\Program Files (x86)\Ookla\output.txt"
 
 try {
-    # this works if I run the file, but if task scheduler does it .\speedtest.exe isn't recognised
     $result = speedtest.exe -f csv
-    Write-EventLog -LogName Application -Source "Speedtest" -EntryType Information -EventId 2 -Message "Executable executed"
 }
 catch {
     Write-EventLog -LogName Application -Source "Speedtest" -EntryType Error -EventId 2 -Message "Error performing speed test: $_"
 }
-
 
 $date = Get-Date
 $dateWithFormat = "`"$date`","
@@ -22,7 +22,6 @@ $datedResult = -join($dateWithFormat, $result)
 
 try {
     Add-Content -Path $resultsFile -Value $datedResult
-    Write-EventLog -LogName Application -Source "Speedtest" -EntryType Information -EventId 2 -Message "Wrote to file"
 }
 catch {
     Write-EventLog -LogName Application -Source "Speedtest" -EntryType Error -EventId 2 -Message "Error writing to file: $_"
