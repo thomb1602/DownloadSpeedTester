d9dae52e10bfede1bc94a9a1b2bf888ab9748ab5
Tom Bates <thomas1602@hotmail.co.uk>
2021-02-16 20:24:17 +0000

    archiver clears results file

--- OutputArchiver.ps1
+++ OutputArchiver.ps1
@@ -1,7 +1,14 @@
 $resultsFile = "C:\Repos\Tom\DownloadSpeedTester\DownloadSpeedTester\useful_output.csv"
-
 $archivePath = "C:\Repos\Tom\DownloadSpeedTester\DownloadSpeedTester\ResultsArchive\output_.csv"
-$date = Get-Date -Format "yyyy-MM-ddTHHmmss"
-$datedArchivePath = $archivePath.Replace('_', $date)
 
-Copy-Item -Path $resultsFile -Destination $datedArchivePath 
+# copy output to archive
+$date = Get-Date -Format "yyyy-MM-ddTHHmmss"
+$newFilePath = $archivePath.Replace('_', $date)
+
+# Copy-Item -Path $resultsFile -Destination $newFilePath 
+
+# empty file and re-add header
+$headerRow = Get-Content -Path $resultsFile -TotalCount 1
+
+Clear-Content -Path $resultsFile
+Add-Content -Path $resultsFile -Value $headerRow
