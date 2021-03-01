f7f052635c714ba46ed725ee3b7a57c75863f833
thomb1602 <41588377+thomb1602@users.noreply.github.com>
2021-03-01 19:16:06 +0000

    up to date with dell server
    has logging and adds filenames to to index.txt

--- OutputArchiver.ps1
+++ OutputArchiver.ps1
@@ -1,13 +1,22 @@
 $resultsFile = "C:\Repos\Tom\DownloadSpeedTester\DownloadSpeedTester\useful_output.csv"
 $archivePath = "C:\Repos\Tom\DownloadSpeedTester\DownloadSpeedTester\ResultsArchive\output_.csv"
+$indexFile =  "C:\Users\Tom\Desktop\speedtest\public\ResultsArchive\index.txt"
+
+Write-EventLog -LogName Application -Source "SpeedtestArchiver" -EntryType Information -EventId 2 -Message "Archiver started"
 
 # copy output to archive
 $date = Get-Date -Format "yyyy-MM-ddTHHmmss"
 $newFilePath = $archivePath.Replace('_', $date)
-
 Copy-Item -Path $resultsFile -Destination $newFilePath 
+Write-EventLog -LogName Application -Source "SpeedtestArchiver" -EntryType Information -EventId 2 -Message "Copied to $newFilePath"
+
+# add filename to index.txt
+$newFileName = $newFilePath.Substring(53);
+Add-Content -Path $indexFile -Value $newFileName
 
 # empty file and re-add header
 $headerRow = Get-Content -Path $resultsFile -TotalCount 1
 Clear-Content -Path $resultsFile
 Add-Content -Path $resultsFile -Value $headerRow
+
+Write-EventLog -LogName Application -Source "SpeedtestArchiver" -EntryType Information -EventId 2 -Message "Empied output.csv"
