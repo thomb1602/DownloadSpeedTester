1025baed2dbc0075e15d4c5b4a9986220c9b14e8
Tom Bates <thomas1602@hotmail.co.uk>
2021-03-03 20:36:56 +0000

    archives and adds to index wifi and internet

--- OutputArchiver.ps1
+++ OutputArchiver.ps1
@@ -1,5 +1,7 @@
-$resultsFile = "C:\Repos\Tom\DownloadSpeedTester\DownloadSpeedTester\useful_output.csv"
-$archivePath = "C:\Repos\Tom\DownloadSpeedTester\DownloadSpeedTester\ResultsArchive\output_.csv"
+$resultsFile = "C:\Users\Tom\Desktop\speedtest\public\output.csv"
+$wifiFile = "C:\Users\Tom\Desktop\speedtest\public\wifi.csv"
+$ethernetFile = "C:\Users\Tom\Desktop\speedtest\public\ethernet.csv"
+$archivePath = "C:\Users\Tom\Desktop\speedtest\public\ResultsArchive\output_.csv"
 $indexFile =  "C:\Users\Tom\Desktop\speedtest\public\ResultsArchive\index.txt"
 
 Write-EventLog -LogName Application -Source "SpeedtestArchiver" -EntryType Information -EventId 2 -Message "Archiver started"
@@ -7,16 +9,22 @@ Write-EventLog -LogName Application -Source "SpeedtestArchiver" -EntryType Infor
 # copy output to archive
 $date = Get-Date -Format "yyyy-MM-ddTHHmmss"
 $newFilePath = $archivePath.Replace('_', $date)
+
 Copy-Item -Path $resultsFile -Destination $newFilePath 
 Write-EventLog -LogName Application -Source "SpeedtestArchiver" -EntryType Information -EventId 2 -Message "Copied to $newFilePath"
 
-# add filename to index.txt
+# add filenames to index.txt
 $newFileName = $newFilePath.Substring(53);
 Add-Content -Path $indexFile -Value $newFileName
+# TODO: add wifi and ethernet filenames to index files
 
-# empty file and re-add header
+# empty files and re-add headers
 $headerRow = Get-Content -Path $resultsFile -TotalCount 1
-Clear-Content -Path $resultsFile
+Clear-Content -Path $resultsFile # output.csv
 Add-Content -Path $resultsFile -Value $headerRow
+Clear-Content -Path $wifiFile # wifi.csv
+Add-Content -Path $wifiFile -Value $headerRow
+Clear-Content -Path $ethernetFile # ethernet.csv
+Add-Content -Path $ethernet -Value $headerRow
 
 Write-EventLog -LogName Application -Source "SpeedtestArchiver" -EntryType Information -EventId 2 -Message "Empied output.csv"
